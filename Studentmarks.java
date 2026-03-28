0fcc6b23294fe1f95620013592a6c95af542da89
ARYA G NAIR <aryagnair.arya@gmail.com>
2026-03-28 15:45:58 +0530

    Assignment3

---
+++ Studentmarks.java
@@ -0,0 +1,24 @@
+package assignment;
+
+public class Studentmarks {
+
+	public static void main(String[] args) {
+		
+		//program 1
+		float marks=87.6f;
+		
+		double marksdouble=marks;
+		
+		int marksint=(int) marksdouble;
+		
+		char markschar = (char) marksint;
+		 
+		System.out.println(marks);
+		System.out.println(marksdouble);
+        System.out.println(marksint);
+        System.out.println(markschar);
+        
+        
+	}
+
+}
