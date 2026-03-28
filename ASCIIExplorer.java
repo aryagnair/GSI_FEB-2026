aa6af7e410034a24022cf64a61d4f5ffe5f4cb58
ARYA G NAIR <aryagnair.arya@gmail.com>
2026-03-28 15:50:07 +0530

    Assignment4

---
+++ ASCIIExplorer.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class ASCIIExplorer {
+
+	public static void main(String[] args) {
+		 char value='A';
+		 int valueint= value;
+		 System.out.println(valueint);
+		  
+		 valueint=valueint+5;
+		 System.out.println(valueint);
+		 
+		 char charval=(char) valueint;
+		 
+		 System.out.println(charval);
+	}
+
+}
